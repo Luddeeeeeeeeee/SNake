fbef73345de2c2fead2eab59385bd21e0209472e
Ludwig Warstedt <ludwig.warstedt@outlook.com>
2024-01-23 15:51:09 +0100

    har gjort så att player_head rör på sig

--- Player.py
+++ Player.py
@@ -3,11 +3,37 @@ import pygame
 
 
 class player(pygame.sprite.Sprite):
-    def __init__(self,x) -> None:
+    def __init__(self,x,b_or_h,) -> None:
         super().__init__()
         self.image = pygame.Surface([40,40])
         self.image.fill((0,100,0))
         self.rect = self.image.get_rect()
-        self.rect = x,350
+        self.rect.x = x
+        self.rect.y = 350
+        self.b_or_h = b_or_h
+
         
         
+    def move(self,dircetion) -> None:
+        if dircetion == "RIGHT":
+            self.rect.x += 10
+        if dircetion == "UP":
+            self.rect.y -= 10
+        if dircetion == "DOWN":
+            self.rect.y += 10
+        if dircetion == "LEFT":
+            self.rect.x -= 10
+
+
+    def update(self,where_to_turn_x,where_to_turn_y) -> None:
+        if self.b_or_h == 1:
+            
+            
+            if self.rect.x == where_to_turn_x:
+                self.rect.y -= 10
+            if self.rect.y == where_to_turn_y:
+                self.rect.x += 10
+
+            else:
+                self.rect.x += 10
+
--- Snake.py
+++ Snake.py
@@ -5,21 +5,37 @@ pygame.init()
 class Game:
 
     def __init__(self):
+        self.dircetion = "RIGHT"
+        self.where_to_turn_x = None
+        self.where_to_turn_y = None
+        self.player_sprite = player(350,(2),)
+        self.player_head = pygame.sprite.GroupSingle(self.player_sprite)
+        self.player_body = pygame.sprite.Group()
+        self.amount = 2
+        self.g = 2
 
-        self.player_sprite = player(350)
-        self.player = pygame.sprite.Group(self.player_sprite)
-        self.amount = 3
     def drawing_player(self):
-        x = 350
-        for t in range(self.amount):
-            x -= 40
-            player_sprite = player(x)
-            self.player.add(player_sprite)
+        x = self.player_head.sprite.rect.x - 40
+        t = 0
+        
+        if self.g >= 0:
+            for t in range(self.amount):
+                player_sprite = player(x,(1),)
+                self.player_body.add(player_sprite)
+                x = self.player_body.sprites()[t].rect.x - 40
+                t += 1
+                self.g -= 1
 
-
+    def uppdate_player(self):
+        self.player_head.sprite.move(self.dircetion)
     def run(self):
+
+        
+        self.player_head.draw(screen)
+        self.player_body.draw(screen)
+        self.player_body.update(self.where_to_turn_x,self.where_to_turn_y)
+        self.uppdate_player()
         self.drawing_player()
-        self.player.draw(screen)
 
 
 
@@ -31,7 +47,6 @@ screen = pygame.display.set_mode((cell_size * cell_number,cell_size * cell_numbe
 run = True
 
 game = Game()
-
 while run:
     
     for event in pygame.event.get():
@@ -39,8 +54,24 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                game.dircetion = "UP"
+                game.where_to_turn_x = game.player_head.sprite.rect.x
 
+            if event.key == pygame.K_s:
+                game.dircetion = "DOWN"
+                game.where_to_turn_x = game.player_head.sprite.rect.x
+
+            if event.key == pygame.K_d:
+                game.dircetion = "RIGHT"
+                game.where_to_turn_y = game.player_head.sprite.rect.y
+
+            if event.key == pygame.K_a:
+                game.dircetion = "LEFT"
+                game.where_to_turn_y = game.player_head.sprite.rect.y
+                
     screen.fill((0,0,0))
     game.run()
     pygame.display.flip()
-    Clock.tick(60) 
+    Clock.tick(11) 
